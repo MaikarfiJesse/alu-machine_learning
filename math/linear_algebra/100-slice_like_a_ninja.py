4eba0ebffeeefe175c52186bbcd85148371b6d82
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-09-11 23:32:08 +0100

    Slice Like A Ninja

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A function that slices a matrix alog specific axes""
+
+
+def np_slice(matrix, axes={}):
+    """Slice matrix along specified axes"""
+    return matrix[
+        tuple([slice(*axes.get(ax, (None, None)))
+               for ax in range(max(axes)+1)])]
