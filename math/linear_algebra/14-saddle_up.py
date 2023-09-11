72d91a916c8996ebaf63fa63fa336d7694cc27ec
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-09-11 23:16:24 +0100

    Saddle Up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A fuction that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Perform matrix multiplication between mat1 and mat2 in that order"""
+    return np.matmul(mat1, mat2)
