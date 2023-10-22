5730cfb22772ac96d1f659bd0d17a4d442607d7d
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-10-22 14:41:48 +0100

    PDF

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Create a class MultiNormal that represents a Multivariate Normal Distribution
+"""
+Create a class MultiNormal that represents a Multivariate Normal Distribution
 """
 
 
@@ -41,3 +42,30 @@ class MultiNormal:
         self.mean = mean
         cov = np.matmul(data - mean, data.T - mean.T) / (n - 1)
         self.cov = cov
+
+    def pdf(self, x):
+        """Calculating the PDF at a data point
+
+        parameters:
+            x [numpy.ndarray of shape (d, 1)]:
+                contains the data point whose PDF should be calculated
+                d: number of dimensions of the instance
+
+        returns:
+            value of the PDF
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        test_d, one = x.shape
+        if test_d != d or one != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        pdf = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        mult = np.matmul(np.matmul((x - self.mean).T, inv), (x - self.mean))
+        pdf *= np.exp(-0.5 * mult)
+        pdf = pdf[0][0]
+        return pdf
