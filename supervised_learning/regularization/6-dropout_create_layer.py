77b5f8af1a55b9b89e7dc926cf9f13e6493bac7d
MaikarfiJesse <j.maikarfi1@alustudent.com>
2024-02-02 17:45:18 +0100

    Create a Layer with Dropout

---
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Write a function that creates a layer of a neural network using dropout"""
+
+import tensorflow as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """ placeholder"""
+    pass
