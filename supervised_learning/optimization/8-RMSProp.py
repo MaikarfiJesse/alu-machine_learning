8f32637fd96111c08dd14ec83bf5bad916847402
MaikarfiJesse <j.maikarfi1@alustudent.com>
2024-02-09 16:56:14 +0100

    RMSProp Upgraded

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Write the function that creates the training operation
+for a neural network in TensorFlow using
+the RMSProp optimization algorithm"""
+
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    Creates the training operation for a neural network in TensorFlow
+        using the RMSProp optimization algorithm
+
+    parameters:
+        loss: the loss of the network
+        alpha [float]: learning rate
+        beta2 [float]: RMSProp weight
+        epsilon [float]: small number to avoid division by zero
+
+    returns:
+        the RMSProp optimization operation
+    """
+    op = tf.train.RMSPropOptimizer(
+        alpha, decay=beta2, epsilon=epsilon).minimize(loss)
+    return op
