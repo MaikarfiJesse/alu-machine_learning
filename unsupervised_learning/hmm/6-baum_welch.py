b2f841ffc73cd753a1f53572fe54b4af3e48b78c
MaikarfiJesse <j.maikarfi1@alustudent.com>
2024-07-04 11:07:00 -0400

    changes

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -5,6 +5,7 @@ Hidden Markov Model"""
 
 import numpy as np
 
+
 def baum_welch(Observation, Transition, Emission, Initial, iterations=1000):
     """
     Performs the Baum-Welch algorithm for a Hidden Markov Model
